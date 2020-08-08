047784755c5c2020b5aea60c9b508fc830d3d088
Markiian benovskyi <admin@markiian-benovskyi.com>
2020-08-08 21:48:24 +0100

    Added meta info message

---
+++ src/app.h
@@ -0,0 +1,16 @@
+//
+// Created by Markiian Benovskyi on 08/08/2020.
+//
+
+#ifndef CASSOWARY_6502_APP_H
+#define CASSOWARY_6502_APP_H
+
+// Define meta for this compiler
+#define COMPILER_NAME    "Cassowary-C Compiler"
+#define COMPILER_DESC    "created for Ben Eater's W65C02 computer project"
+#define COMPILER_VERSION "0.0.1-alpha"
+#define COMPILER_AUTHOR  "Markiian Benovskyi"
+#define COMPILER_PAGE    "https://github.com/MarkusBansky/cassowary-6502"
+#define COMPILER_EATER   "https://eater.net/6502"
+
+#endif //CASSOWARY_6502_APP_H
--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
+//
+// Created by Markiian Benovskyi on 08/08/2020.
+//
+
 #include <stdio.h>
+#include <time.h>
+#include "app.h"
+
+void print_header() {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    printf("~~~~~ [%s] [%s] ~~~~~\n", COMPILER_NAME, COMPILER_VERSION);
+    printf("[cassowary] %s\n", COMPILER_DESC);
+    printf("[cassowary] (c) 2020-%d by %s\n", tm.tm_year + 1900, COMPILER_AUTHOR);
+    printf("[cassowary] version %s\n", COMPILER_VERSION);
+    printf("[cassowary] %s\n", COMPILER_EATER);
+    printf("[cassowary] %s\n", COMPILER_PAGE);
+    printf("\n");
+}
 
 int main() {
-    printf("Hello, World!\n");
+    print_header();
+
     return 0;
 }
