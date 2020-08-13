0ab2316d3f086bb1733b9b9304e748cfec66a2de
Markiian benovskyi <admin@markiian-benovskyi.com>
2020-08-13 11:08:21 +0100

    Testing

--- examples/hello_world/main.c
+++ examples/hello_world/main.c
@@ -2,11 +2,14 @@
 // Created by Markiian Benovskyi on 10/08/2020.
 //
 
-#include <cassowary_6502.h>x
+//#include <cassowary_6502.h>
 
 int main() {
     while (1) {
-        lcd_init();
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+//        lcd_init();
 //        lcd_send_char("H");
 //        lcd_print("Hello world!");
     }
